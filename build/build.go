e6b991abb9528cbb6d9236c2033528dfd5e3c931
Tonis Tiigi <tonistiigi@gmail.com>
2019-04-19 10:00:35 -0700

    WIP build: multi-driver build
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- build/build.go
+++ build/build.go
@@ -20,7 +20,6 @@ import (
 	"github.com/moby/buildkit/session/upload/uploadprovider"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"github.com/tonistiigi/buildx/driver"
 	"github.com/tonistiigi/buildx/util/progress"
 	"golang.org/x/sync/errgroup"
@@ -83,38 +82,233 @@ func getFirstDriver(drivers []DriverInfo) (driver.Driver, error) {
 	return nil, err
 }
 
+func filterAvailableDrivers(drivers []DriverInfo) ([]DriverInfo, error) {
+	out := make([]DriverInfo, 0, len(drivers))
+	err := errors.Errorf("no drivers found")
+	for _, di := range drivers {
+		if di.Err == nil && di.Driver != nil {
+			out = append(out, di)
+		}
+		if di.Err != nil {
+			err = di.Err
+		}
+	}
+	if len(out) > 0 {
+		return out, nil
+	}
+	return nil, err
+}
+
+type driverPair struct {
+	driverIndex int
+	platforms   []specs.Platform
+}
+
+func driverIndexes(m map[string][]driverPair) []int {
+	out := make([]int, 0, len(m))
+	visited := map[int]struct{}{}
+	for _, dp := range m {
+		for _, d := range dp {
+			if _, ok := visited[d.driverIndex]; ok {
+				continue
+			}
+			visited[d.driverIndex] = struct{}{}
+			out = append(out, d.driverIndex)
+		}
+	}
+	return out
+}
+
+func allIndexes(l int) []int {
+	out := make([]int, 0, l)
+	for i := 0; i < l; i++ {
+		out = append(out, i)
+	}
+	return out
+}
+
+func ensureBooted(ctx context.Context, drivers []DriverInfo, idxs []int, pw progress.Writer) ([]*client.Client, error) {
+	clients := make([]*client.Client, len(drivers))
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for _, i := range idxs {
+		func(i int) {
+			eg.Go(func() error {
+				c, err := driver.Boot(ctx, drivers[i].Driver, pw)
+				if err != nil {
+					return err
+				}
+				clients[i] = c
+				return nil
+			})
+		}(i)
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
+	return clients, nil
+}
+
+func splitToDriverPairs(availablePlatforms map[string]int, opt map[string]Options) map[string][]driverPair {
+	m := map[string][]driverPair{}
+	for k, opt := range opt {
+		mm := map[int][]specs.Platform{}
+		for _, p := range opt.Platforms {
+			k := platforms.Format(p)
+			idx := availablePlatforms[k] // default 0
+			pp := mm[idx]
+			pp = append(pp, p)
+			mm[idx] = pp
+		}
+		dps := make([]driverPair, 0, 2)
+		for idx, pp := range mm {
+			dps = append(dps, driverPair{driverIndex: idx, platforms: pp})
+		}
+		m[k] = dps
+	}
+	return m
+}
+
+func resolveDrivers(ctx context.Context, drivers []DriverInfo, opt map[string]Options, pw progress.Writer) (map[string][]driverPair, []*client.Client, error) {
+
+	availablePlatforms := map[string]int{}
+	for i, d := range drivers {
+		for _, p := range d.Platform {
+			availablePlatforms[platforms.Format(p)] = i
+		}
+	}
+
+	undetectedPlatform := false
+	allPlatforms := map[string]int{}
+	for _, opt := range opt {
+		for _, p := range opt.Platforms {
+			k := platforms.Format(p)
+			allPlatforms[k] = -1
+			if _, ok := availablePlatforms[k]; !ok {
+				undetectedPlatform = true
+			}
+		}
+	}
+
+	// fast path
+	if len(drivers) == 1 || len(allPlatforms) == 0 {
+		m := map[string][]driverPair{}
+		for k, opt := range opt {
+			m[k] = []driverPair{{driverIndex: 0, platforms: opt.Platforms}}
+		}
+		clients, err := ensureBooted(ctx, drivers, driverIndexes(m), pw)
+		if err != nil {
+			return nil, nil, err
+		}
+		return m, clients, nil
+	}
+
+	// map based on existing platforms
+	if !undetectedPlatform {
+		// for k := range opt {
+		// 	mm := map[int][]specs.Platform{}
+		// 	for _, p := range opt.Platforms {
+		// 		k := platforms.Format(p)
+		// 		idx := availablePlatforms[k]
+		// 		pp := mm[idx]
+		// 		pp = append(pp, p)
+		// 		mm[idx] = pp
+		// 	}
+		// 	dps := make([]driverPair, 0, 2)
+		// 	for idx, pp := range mm {
+		// 		dps = append(dps, driverPair{driverIndex: idx, platforms: pp})
+		// 	}
+		// 	m[k] = dps
+		// }
+		m := splitToDriverPairs(availablePlatforms, opt)
+		clients, err := ensureBooted(ctx, drivers, driverIndexes(m), pw)
+		if err != nil {
+			return nil, nil, err
+		}
+		return m, clients, nil
+	}
+
+	// boot all drivers in k
+	clients, err := ensureBooted(ctx, drivers, allIndexes(len(drivers)), pw)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	workers := make([][]*client.WorkerInfo, len(clients))
+
+	for i, c := range clients {
+		if c == nil {
+			continue
+		}
+		func(i int) {
+			eg.Go(func() error {
+				ww, err := clients[i].ListWorkers(ctx)
+				if err != nil {
+					return errors.Wrap(err, "listing workers")
+				}
+				workers[i] = ww
+				return nil
+			})
+		}(i)
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, nil, err
+	}
+
+	for i, ww := range workers {
+		for _, w := range ww {
+			for _, p := range w.Platforms {
+				p = platforms.Normalize(p)
+				ps := platforms.Format(p)
+
+				if _, ok := availablePlatforms[ps]; !ok {
+					availablePlatforms[ps] = i
+				}
+			}
+		}
+	}
+
+	return splitToDriverPairs(availablePlatforms, opt), clients, nil
+}
+
+func numDrivers(m map[string][]driverPair) int {
+	mm := map[int]struct{}{}
+	for _, v := range m {
+		for _, dp := range v {
+			if _, ok := mm[dp.driverIndex]; !ok {
+				mm[dp.driverIndex] = struct{}{}
+			}
+		}
+	}
+	return len(mm)
+}
+
 func Build(ctx context.Context, drivers []DriverInfo, opt map[string]Options, docker DockerAPI, pw progress.Writer) (map[string]*client.SolveResponse, error) {
 	if len(drivers) == 0 {
 		return nil, errors.Errorf("driver required for build")
 	}
 
-	if len(drivers) > 1 {
-		return nil, errors.Errorf("multiple drivers currently not supported")
-	}
-
-	d, err := getFirstDriver(drivers)
+	drivers, err := filterAvailableDrivers(drivers)
 	if err != nil {
-		return nil, err
-	}
-	_, isDefaultMobyDriver := d.(interface {
-		IsDefaultMobyDriver()
-	})
-
-	for _, opt := range opt {
-		if !isDefaultMobyDriver && len(opt.Exports) == 0 {
-			logrus.Warnf("No output specified for %s driver. Build result will only remain in the build cache. To push result image into registry use --push or to load image into docker use --load", d.Factory().Name())
-			break
-		}
+		return nil, errors.Wrapf(err, "no valid drivers found")
 	}
 
-	c, err := driver.Boot(ctx, d, pw)
+	m, clients, err := resolveDrivers(ctx, drivers, opt, pw)
 	if err != nil {
 		close(pw.Status())
 		<-pw.Done()
 		return nil, err
 	}
 
-	withPrefix := len(opt) > 1
+	exportsWarned := false
+	multiDriver := numDrivers(m) > 1
+	multiTarget := len(opt) > 1
+	withPrefix := multiTarget || multiDriver
 
 	mw := progress.NewMultiWriter(pw)
 
@@ -123,188 +317,228 @@ func Build(ctx context.Context, drivers []DriverInfo, opt map[string]Options, do
 	resp := map[string]*client.SolveResponse{}
 	var mu sync.Mutex
 
+	var defers []func()
+	defer func() {
+		for _, f := range defers {
+			f()
+		}
+	}()
+
 	for k, opt := range opt {
-		pw := mw.WithPrefix(k, withPrefix)
-
-		if opt.ImageIDFile != "" {
-			if len(opt.Platforms) != 0 {
-				return nil, errors.Errorf("image ID file cannot be specified when building for multiple platforms")
-			}
-			// Avoid leaving a stale file if we eventually fail
-			if err := os.Remove(opt.ImageIDFile); err != nil && !os.IsNotExist(err) {
-				return nil, errors.Wrap(err, "removing image ID file")
-			}
-		}
-
-		if v, ok := opt.BuildArgs["BUILDKIT_INLINE_CACHE"]; ok {
-			if v, _ := strconv.ParseBool(v); v {
-				opt.CacheTo = append(opt.CacheTo, client.CacheOptionsEntry{
-					Type:  "inline",
-					Attrs: map[string]string{},
+		for _, dp := range m[k] {
+			err := func() error {
+				di := drivers[dp.driverIndex]
+				d := di.Driver
+				c := clients[dp.driverIndex]
+				_, isDefaultMobyDriver := d.(interface {
+					IsDefaultMobyDriver()
 				})
-			}
-		}
 
-		for _, e := range opt.CacheTo {
-			if e.Type != "inline" && !d.Features()[driver.CacheExport] {
-				return nil, notSupported(d, driver.CacheExport)
-			}
-		}
-
-		so := client.SolveOpt{
-			Frontend:      "dockerfile.v0",
-			FrontendAttrs: map[string]string{},
-			LocalDirs:     map[string]string{},
-			CacheExports:  opt.CacheTo,
-			CacheImports:  opt.CacheFrom,
-		}
-
-		switch len(opt.Exports) {
-		case 1:
-			// valid
-		case 0:
-			if isDefaultMobyDriver {
-				// backwards compat for docker driver only:
-				// this ensures the build results in a docker image.
-				opt.Exports = []client.ExportEntry{{Type: "image", Attrs: map[string]string{}}}
-			}
-		default:
-			return nil, errors.Errorf("multiple outputs currently unsupported")
-		}
-
-		if len(opt.Tags) > 0 {
-			tags := make([]string, len(opt.Tags))
-			for i, tag := range opt.Tags {
-				ref, err := reference.Parse(tag)
-				if err != nil {
-					return nil, errors.Wrapf(err, "invalid tag %q", tag)
+				if !isDefaultMobyDriver && len(opt.Exports) == 0 && !exportsWarned {
+					// TODO
+					// logrus.Warnf("No output specified for %s driver. Build result will only remain in the build cache. To push result image into registry use --push or to load image into docker use --load", d.Factory().Name())
+					exportsWarned = true
 				}
-				tags[i] = ref.String()
-			}
-			for i, e := range opt.Exports {
-				switch e.Type {
-				case "image", "oci", "docker":
-					opt.Exports[i].Attrs["name"] = strings.Join(tags, ",")
-				}
-			}
-		} else {
-			for _, e := range opt.Exports {
-				if e.Type == "image" && e.Attrs["name"] == "" && e.Attrs["push"] != "" {
-					if ok, _ := strconv.ParseBool(e.Attrs["push"]); ok {
-						return nil, errors.Errorf("tag is needed when pushing to registry")
-					}
-				}
-			}
-		}
 
-		for i, e := range opt.Exports {
-			if (e.Type == "local" || e.Type == "tar") && opt.ImageIDFile != "" {
-				return nil, errors.Errorf("local and tar exporters are incompatible with image ID file")
-			}
-			if e.Type == "oci" && !d.Features()[driver.OCIExporter] {
-				return nil, notSupported(d, driver.OCIExporter)
-			}
-			if e.Type == "docker" {
-				if e.Output == nil {
-					if isDefaultMobyDriver {
-						e.Type = "image"
+				kk := k
+				if multiDriver {
+					if multiTarget {
+						kk += di.Name + " "
 					} else {
-						w, cancel, err := newDockerLoader(ctx, docker, e.Attrs["context"], mw)
+						kk = di.Name
+					}
+				}
+
+				pw := mw.WithPrefix(kk, withPrefix)
+
+				pl := dp.platforms
+
+				if opt.ImageIDFile != "" {
+					if len(pl) != 0 {
+						return errors.Errorf("image ID file cannot be specified when building for multiple platforms")
+					}
+					// Avoid leaving a stale file if we eventually fail
+					if err := os.Remove(opt.ImageIDFile); err != nil && !os.IsNotExist(err) {
+						return errors.Wrap(err, "removing image ID file")
+					}
+				}
+
+				if v, ok := opt.BuildArgs["BUILDKIT_INLINE_CACHE"]; ok {
+					if v, _ := strconv.ParseBool(v); v {
+						opt.CacheTo = append(opt.CacheTo, client.CacheOptionsEntry{
+							Type:  "inline",
+							Attrs: map[string]string{},
+						})
+					}
+				}
+
+				for _, e := range opt.CacheTo {
+					if e.Type != "inline" && !d.Features()[driver.CacheExport] {
+						return notSupported(d, driver.CacheExport)
+					}
+				}
+
+				so := client.SolveOpt{
+					Frontend:      "dockerfile.v0",
+					FrontendAttrs: map[string]string{},
+					LocalDirs:     map[string]string{},
+					CacheExports:  opt.CacheTo,
+					CacheImports:  opt.CacheFrom,
+				}
+
+				switch len(opt.Exports) {
+				case 1:
+					// valid
+				case 0:
+					if isDefaultMobyDriver {
+						// backwards compat for docker driver only:
+						// this ensures the build results in a docker image.
+						opt.Exports = []client.ExportEntry{{Type: "image", Attrs: map[string]string{}}}
+					}
+				default:
+					return errors.Errorf("multiple outputs currently unsupported")
+				}
+
+				if len(opt.Tags) > 0 {
+					tags := make([]string, len(opt.Tags))
+					for i, tag := range opt.Tags {
+						ref, err := reference.Parse(tag)
 						if err != nil {
-							return nil, err
+							return errors.Wrapf(err, "invalid tag %q", tag)
 						}
-						defer cancel()
-						opt.Exports[i].Output = w
+						tags[i] = ref.String()
 					}
-				} else if !d.Features()[driver.DockerExporter] {
-					return nil, notSupported(d, driver.DockerExporter)
-				}
-			}
-			if e.Type == "image" && isDefaultMobyDriver {
-				opt.Exports[i].Type = "moby"
-				if e.Attrs["push"] != "" {
-					if ok, _ := strconv.ParseBool(e.Attrs["push"]); ok {
-						return nil, errors.Errorf("auto-push is currently not implemented for docker driver")
+					for i, e := range opt.Exports {
+						switch e.Type {
+						case "image", "oci", "docker":
+							opt.Exports[i].Attrs["name"] = strings.Join(tags, ",")
+						}
+					}
+				} else {
+					for _, e := range opt.Exports {
+						if e.Type == "image" && e.Attrs["name"] == "" && e.Attrs["push"] != "" {
+							if ok, _ := strconv.ParseBool(e.Attrs["push"]); ok {
+								return errors.Errorf("tag is needed when pushing to registry")
+							}
+						}
 					}
 				}
-			}
-		}
 
-		// TODO: handle loading to docker daemon
+				for i, e := range opt.Exports {
+					if (e.Type == "local" || e.Type == "tar") && opt.ImageIDFile != "" {
+						return errors.Errorf("local and tar exporters are incompatible with image ID file")
+					}
+					if e.Type == "oci" && !d.Features()[driver.OCIExporter] {
+						return notSupported(d, driver.OCIExporter)
+					}
+					if e.Type == "docker" {
+						if e.Output == nil {
+							if isDefaultMobyDriver {
+								e.Type = "image"
+							} else {
+								w, cancel, err := newDockerLoader(ctx, docker, e.Attrs["context"], mw)
+								if err != nil {
+									return err
+								}
+								defers = append(defers, cancel)
+								opt.Exports[i].Output = w
+							}
+						} else if !d.Features()[driver.DockerExporter] {
+							return notSupported(d, driver.DockerExporter)
+						}
+					}
+					if e.Type == "image" && isDefaultMobyDriver {
+						opt.Exports[i].Type = "moby"
+						if e.Attrs["push"] != "" {
+							if ok, _ := strconv.ParseBool(e.Attrs["push"]); ok {
+								return errors.Errorf("auto-push is currently not implemented for docker driver")
+							}
+						}
+					}
+				}
 
-		so.Exports = opt.Exports
-		so.Session = opt.Session
+				// TODO: handle loading to docker daemon
 
-		release, err := LoadInputs(opt.Inputs, &so)
-		if err != nil {
-			return nil, err
-		}
-		defer release()
+				so.Exports = opt.Exports
+				so.Session = opt.Session
 
-		if opt.Pull {
-			so.FrontendAttrs["image-resolve-mode"] = "pull"
-		}
-		if opt.Target != "" {
-			so.FrontendAttrs["target"] = opt.Target
-		}
-		if opt.NoCache {
-			so.FrontendAttrs["no-cache"] = ""
-		}
-		for k, v := range opt.BuildArgs {
-			so.FrontendAttrs["build-arg:"+k] = v
-		}
-		for k, v := range opt.Labels {
-			so.FrontendAttrs["label:"+k] = v
-		}
+				release, err := LoadInputs(opt.Inputs, &so)
+				if err != nil {
+					return err
+				}
+				defers = append(defers, release)
 
-		if len(opt.Platforms) != 0 {
-			pp := make([]string, len(opt.Platforms))
-			for i, p := range opt.Platforms {
-				pp[i] = platforms.Format(p)
-			}
-			if len(pp) > 1 && !d.Features()[driver.MultiPlatform] {
-				return nil, notSupported(d, driver.MultiPlatform)
-			}
-			so.FrontendAttrs["platform"] = strings.Join(pp, ",")
-		}
+				if opt.Pull {
+					so.FrontendAttrs["image-resolve-mode"] = "pull"
+				}
+				if opt.Target != "" {
+					so.FrontendAttrs["target"] = opt.Target
+				}
+				if opt.NoCache {
+					so.FrontendAttrs["no-cache"] = ""
+				}
+				for k, v := range opt.BuildArgs {
+					so.FrontendAttrs["build-arg:"+k] = v
+				}
+				for k, v := range opt.Labels {
+					so.FrontendAttrs["label:"+k] = v
+				}
 
-		switch opt.NetworkMode {
-		case "host", "none":
-			so.FrontendAttrs["force-network-mode"] = opt.NetworkMode
-		case "", "default":
-		default:
-			return nil, errors.Errorf("network mode %q not supported by buildkit", opt.NetworkMode)
-		}
+				if len(pl) != 0 {
+					pp := make([]string, len(pl))
+					for i, p := range pl {
+						pp[i] = platforms.Format(p)
+					}
+					if len(pp) > 1 && !d.Features()[driver.MultiPlatform] {
+						return notSupported(d, driver.MultiPlatform)
+					}
+					so.FrontendAttrs["platform"] = strings.Join(pp, ",")
+				}
 
-		extraHosts, err := toBuildkitExtraHosts(opt.ExtraHosts)
-		if err != nil {
-			return nil, err
-		}
-		so.FrontendAttrs["add-hosts"] = extraHosts
+				switch opt.NetworkMode {
+				case "host", "none":
+					so.FrontendAttrs["force-network-mode"] = opt.NetworkMode
+				case "", "default":
+				default:
+					return errors.Errorf("network mode %q not supported by buildkit", opt.NetworkMode)
+				}
 
-		var statusCh chan *client.SolveStatus
-		if pw != nil {
-			pw = progress.ResetTime(pw)
-			statusCh = pw.Status()
-			eg.Go(func() error {
-				<-pw.Done()
-				return pw.Err()
-			})
-		}
+				extraHosts, err := toBuildkitExtraHosts(opt.ExtraHosts)
+				if err != nil {
+					return err
+				}
+				so.FrontendAttrs["add-hosts"] = extraHosts
 
-		eg.Go(func() error {
-			rr, err := c.Solve(ctx, nil, so, statusCh)
+				var statusCh chan *client.SolveStatus
+				if pw != nil {
+					pw = progress.ResetTime(pw)
+					statusCh = pw.Status()
+					eg.Go(func() error {
+						<-pw.Done()
+						return pw.Err()
+					})
+				}
+
+				iidfile := opt.ImageIDFile
+				eg.Go(func() error {
+					rr, err := c.Solve(ctx, nil, so, statusCh)
+					if err != nil {
+						return err
+					}
+					mu.Lock()
+					resp[k] = rr
+					mu.Unlock()
+					if iidfile != "" {
+						return ioutil.WriteFile(iidfile, []byte(rr.ExporterResponse["containerimage.digest"]), 0644)
+					}
+					return nil
+				})
+				return nil
+			}()
 			if err != nil {
-				return err
+				return nil, err
 			}
-			mu.Lock()
-			resp[k] = rr
-			mu.Unlock()
-			if opt.ImageIDFile != "" {
-				return ioutil.WriteFile(opt.ImageIDFile, []byte(rr.ExporterResponse["containerimage.digest"]), 0644)
-			}
-			return nil
-		})
+		}
 	}
 
 	if err := eg.Wait(); err != nil {
